5130aa3191e87597ff6b2369673e73b45922d757
AbuOmar373 <wzw808@gmail.com>
2018-03-09 21:27:29 +0300

    Convert to MVC design pattem

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by AbuOmar on 21 جما٢، 1439 هـ.
+//  Copyright © 1439 هـ AbuOmar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false 
+}
